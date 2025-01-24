3dfa22325ceb8da6924085896ca97ba1f5649368
= <44247277+javierb256@users.noreply.github.com>
2025-01-24 15:42:50 -0800

    updated create account page to be mobile responsive

--- src/pages/CreateAccount/CreateAccount.tsx
+++ src/pages/CreateAccount/CreateAccount.tsx
@@ -1,5 +1,5 @@
 import styles from "./CreateAccount.module.css";
-import BoxContainer from "../../components/BoxContainer/BoxContainer"
+import BoxContainer from "../../components/BoxContainer/BoxContainer";
 import LinkButton from "../../components/LinkButton/LinkButton";
 import BackButton from "../../components/BackButton/BackButton";
 
@@ -14,26 +14,60 @@ function CreateAccount() {
       <h2 className={`${styles["create-account-h2"]} text-center`}>
         Account Information
       </h2>
-      
+
       <BoxContainer className={`${styles.container}`}>
         <form className={`${styles["account-information-form"]} `}>
-            <div>
-                <label htmlFor="first-name" className="label">First Name</label>
-                <input id="first-name" name="first-name" type="text" className="input"></input>
-            </div>
-            <div>
-                <label htmlFor="last-name" className="label">Last Name</label>
-                <input id="last-name" name="last-name" type="text" className="input"></input>
-            </div>
-            <div>
-                <label htmlFor="password" className="label">Password</label>
-                <input id="password" name="password" type="password" className="input"></input>
-            </div>
-            <div>
-                <label htmlFor="email" className="label">Email</label>
-                <input id="email" name="email" type="email" className="input"></input>
-            </div>
-            <LinkButton to="confirmation" color="scarlet" className="margin-top-2">Create Account</LinkButton>
+          <div>
+            <label htmlFor="first-name" className="label">
+              First Name
+            </label>
+            <input
+              id="first-name"
+              name="first-name"
+              type="text"
+              className="input"
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="last-name" className="label">
+              Last Name
+            </label>
+            <input
+              id="last-name"
+              name="last-name"
+              type="text"
+              className="input"
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="password" className="label">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              className="input"
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="email" className="label">
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              className="input"
+            ></input>
+          </div>
+          <LinkButton
+            to="confirmation"
+            color="scarlet"
+            className="margin-top-2"
+          >
+            Create Account
+          </LinkButton>
         </form>
       </BoxContainer>
     </>
