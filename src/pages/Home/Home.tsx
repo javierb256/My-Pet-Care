7c4c9697092995562c869146b6be5168869dc1cb
= <44247277+javierb256@users.noreply.github.com>
2025-02-05 20:06:58 -0800

    started work on Home page mobie responsiveness on main header

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ import petKennel from "../../assets/dog-crate-icon-white.png";
 import genderIcon from "../../assets/unisex-symbol-icon-white.png";
 import weightIcon from "../../assets/lbs-measure-weight-icon(1).png";
 import ageIcon from "../../assets/age-icon-white.png";
-import pawPrint from "../../assets/paw-icon.png";
+import pawPrint from "../../assets/paw-icon-2.png";
 import calendarIcon from "../../assets/calendar-regular.svg"
 import stickyNote from "../../assets/sticky-note-white.svg"
 import BoxContainer from "../../components/BoxContainer/BoxContainer";
@@ -18,7 +18,7 @@ function Home() {
       <Navbar />
       {/* header section */}
       <section className={styles["home-header"]}>
-        <img src={pawPrint} alt="pet type icon" />
+        <img className={styles["background-image"]} src={pawPrint} alt="pet type icon" />
         <div className={styles["home-pet"]}>
           <h3>Sonic</h3>
           {/* get picture from first time login, then from database later */}
@@ -27,44 +27,47 @@ function Home() {
         <div className={styles["home-pet-facts"]}>
           <h3>My Pet Facts</h3>
           {/* fact container */}
-          <div className={styles["pet-fact-container"]}>
-            <div className={`${styles["icon-container"]} ${styles["blue-background"]}`}>
-              <img src={petKennel} alt="pet kennel" />
+          <div className={styles["pet-fact-flex"]}>
+            <div className={styles["pet-fact-container"]}>
+              <div className={`${styles["icon-container"]} ${styles["blue-background"]}`}>
+                <img src={petKennel} alt="pet kennel" />
+              </div>
+              <div className={styles["fact-details"]}>
+                <p>Species</p>
+                <p>name</p>
+              </div>
             </div>
-            <div>
-              <p>Species</p>
-              <p>name</p>
+            {/* fact container */}
+            <div className={styles["pet-fact-container"]}>
+              <div className={`${styles["icon-container"]} ${styles["red-background"]}`}>
+                <img src={genderIcon} alt="gender icon" />
+              </div>
+              <div className={styles["fact-details"]}>
+                <p>Gender</p>
+                <p>name</p>
+              </div>
             </div>
-          </div>
-          {/* fact container */}
-          <div className={styles["pet-fact-container"]}>
-            <div className={`${styles["icon-container"]} ${styles["red-background"]}`}>
-              <img src={genderIcon} alt="gender icon" />
+            {/* fact container */}
+            <div className={styles["pet-fact-container"]}>
+              <div className={`${styles["icon-container"]} ${styles["green-background"]}`}>
+                <img src={weightIcon} alt="weight icon" />
+              </div>
+              <div className={styles["fact-details"]}>
+                <p>Weight</p>
+                <p>name</p>
+              </div>
             </div>
-            <div>
-              <p>Gender</p>
-              <p>name</p>
+            {/* fact container */}
+            <div className={styles["pet-fact-container"]}>
+              <div className={`${styles["icon-container"]} ${styles["purple-background"]}`}>
+                <img src={ageIcon} alt="age icon" />
+              </div>
+              <div className={styles["fact-details"]}>
+                <p>Age</p>
+                <p>name</p>
+              </div>
             </div>
-          </div>
-          {/* fact container */}
-          <div className={styles["pet-fact-container"]}>
-            <div className={`${styles["icon-container"]} ${styles["green-background"]}`}>
-              <img src={weightIcon} alt="weight icon" />
-            </div>
-            <div>
-              <p>Weight</p>
-              <p>name</p>
-            </div>
-          </div>
-          {/* fact container */}
-          <div className={styles["pet-fact-container"]}>
-            <div className={`${styles["icon-container"]} ${styles["purple-background"]}`}>
-              <img src={ageIcon} alt="age icon" />
-            </div>
-            <div>
-              <p>Age</p>
-              <p>name</p>
-            </div>
+
           </div>
         </div>
       </section>
