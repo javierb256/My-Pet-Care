068fbd39ac05e0111c030a9d290fcf5d88ca3faf
javier <hello@javierbarrera.dev>
2025-02-11 13:47:58 -0800

    fixed routes

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ import Confirmation from './pages/Confirmation/Confirmation.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='My-Pet-Care'>
     <Routes>
       <Route path="/" element={<App />}/>
       {/* <Route path="login" element={<Login />}/> */}
