7313936fd75983a4aadec5b35ffb84dbe0db0d78
javier <hello@javierbarrera.dev>
2025-05-05 20:11:52 -0700

    updated formatDate function to properly handle off by one error

--- src/utilities/utils.tsx
+++ src/utilities/utils.tsx
@@ -2,6 +2,7 @@
 
 export const formatDate = (date:string | Date) => {
     const newDate = new Date(date);
+    newDate.setMinutes(newDate.getMinutes() + newDate.getTimezoneOffset());
     const format = newDate.toLocaleDateString('en-US', {
       day: "numeric",
       month: "long",
