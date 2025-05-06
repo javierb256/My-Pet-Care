e38475734cc0fc9883732b0343195c37a0044eba
javier <hello@javierbarrera.dev>
2025-05-05 21:52:47 -0700

    added base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/My-Pet-Care",
+  // base: "/My-Pet-Care",
+  base: process.VITE_BASE_PATH || "/My-Pet-Care",
 })
