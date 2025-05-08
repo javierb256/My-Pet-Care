72ed51568586a5e28ad7899a1d0a8c6402f624d0
javier <hello@javierbarrera.dev>
2025-05-08 12:36:25 -0700

    updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -24,7 +24,7 @@ createRoot(document.getElementById('root')!).render(
       <Route path="appointments" element={<Appointments />}/>
       <Route path="create-account/confirmation" element={<Confirmation />}/>
       <Route path="account-recovery" element={<AccountRecovery />}/>
-      <Route path="*" element={<NotFound />}></Route>
+      <Route element={<NotFound />}></Route>
     </Routes>
     </BrowserRouter>
   </StrictMode>,
